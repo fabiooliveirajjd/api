ce5ef0c47a09316334fcec5304e8b3a0dcf2ba9a
Fabio Santos Oliveira <fabio.oliveira.prestador>
2023-09-06 11:58:57 -0300

    inicial

---
+++ src/test/java/com/fabio/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabio.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
