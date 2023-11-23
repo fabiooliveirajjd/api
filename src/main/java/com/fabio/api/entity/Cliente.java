8751736e801d830c5760025fbe77a9eeeca97853
Fabio Santos Oliveira <fabio.oliveira.prestador>
2023-11-23 16:34:05 -0300

    att

--- src/main/java/com/fabio/api/entity/Cliente.java
+++ src/main/java/com/fabio/api/entity/Cliente.java
@@ -1,7 +1,9 @@
 package com.fabio.api.entity;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
@@ -12,10 +14,10 @@ import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
+@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity
 @Table(name = "clientes")
 @EntityListeners(AuditingEntityListener.class)
 public class Cliente implements Serializable {
@@ -23,10 +25,13 @@ public class Cliente implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(name = "nome", nullable = false, length = 100)
     private String nome;
+
     @Column(name = "cpf", nullable = false, unique = true, length = 11)
     private String cpf;
+
     @OneToOne
     @JoinColumn(name = "id_usuario", nullable = false)
     private Usuario usuario;
@@ -34,12 +39,15 @@ public class Cliente implements Serializable {
     @CreatedDate
     @Column(name = "data_criacao")
     private LocalDateTime dataCriacao;
+
     @LastModifiedDate
     @Column(name = "data_modificacao")
     private LocalDateTime dataModificacao;
+
     @CreatedBy
     @Column(name = "criado_por")
-    private String criadoPor;
+    private String criadorPor;
+
     @LastModifiedBy
     @Column(name = "modificado_por")
     private String modificadoPor;
