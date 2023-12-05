b6cf438af25f4e0b19da126cbd38bb243c190327
Fabio Santos Oliveira <fabio.oliveira.prestador>
2023-12-05 15:13:00 -0300

    Controller Estacionamento

--- src/main/java/com/fabio/api/repository/VagaRepository.java
+++ src/main/java/com/fabio/api/repository/VagaRepository.java
@@ -5,6 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+import java.util.Optional;
+
 public interface VagaRepository extends JpaRepository<Vaga, Long> {
     Optional<Vaga> findByCodigo(String codigo);
+
+    Optional<Vaga> findFirstByStatus(Vaga.StatusVaga statusVaga);
+
+
 }
